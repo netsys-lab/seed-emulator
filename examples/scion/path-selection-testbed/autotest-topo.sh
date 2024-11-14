d3af8b591b0d91065cffaeb85ec89ad89dfcd3b5
Leon Rinkel <leon@rinkel.me>
2024-11-14 12:19:53 +0100

    Pick strats to run

--- examples/scion/path-selection-testbed/autotest-topo.sh
+++ examples/scion/path-selection-testbed/autotest-topo.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# completelyRandomStrat          = 0
+# firstFreeRandomStrat           = 1
+# firstFreeShortestStrat         = 2
+# firstFreeMostDisjointStrat     = 4
+# singleShortestPathStrat        = 6
+# firstFreeLowestLatency         = 7
+# firstFreeHighestBandwidth      = 8
+# firstFreeLowestLatencySubvalue = 9
+
 for run in `seq 1 20`
 do
-    for strat in `seq 0 8`
+    for strat in 0 1 2 4 6 7 8 9
     do
         rm -rf output
         python testbed-topo.py \
             --path-strategy $strat \
-            --content-size 51200000 \
+            --content-size 10240000 \
             --num-nodes 1 \
             --output-file runs/$RANDOM.log
     done
