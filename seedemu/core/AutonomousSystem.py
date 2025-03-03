e063f70054d8f445d9902d9e3b69a4f20b82ea47
Lars-Christian Schulz <lschulz@ovgu.de>
2025-03-03 15:53:45 +0100

    Update AutonomousSystem.py

--- seedemu/core/AutonomousSystem.py
+++ seedemu/core/AutonomousSystem.py
@@ -15,6 +15,8 @@ import requests
 RIS_PREFIXLIST_URL = "https://stat.ripe.net/data/announced-prefixes/data.json"
 
 
+
+
 class AutonomousSystem(Printable, Graphable, Configurable):
     """!
     @brief AutonomousSystem class.
