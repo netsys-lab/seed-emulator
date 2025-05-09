27d18780a27054bbcbea2a71a41aed4db7049c6f
amdfxlucas <saculolissat@gmx.de>
2025-05-09 11:47:23 +0200

    wip scion-doq

--- examples/scion/S11_scion_doq/scion-doq-dev.py
+++ examples/scion/S11_scion_doq/scion-doq-dev.py
@@ -19,7 +19,50 @@ import os, sys
 from ipaddress import IPv4Network
 from collections import defaultdict
 
+WebSiteContents = {}
+WebSiteContents['index_template'] = '''\
+<h1> {asn}-{nodeName} {domain} {address}</h1>
+<pre>
+{body}
+</pre>
+'''
 
+WebSiteContents['www_example_com'] ='''\
+                                                                                 ___
+                                                                                /\_ \ 
+ __  __  __  __  __  __  __  __  __        __   __  _    __      ___ ___   _____\//\ \      __        ___    ___     ___ ___
+/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \     /'__`\/\ \/'\ /'__`\  /' __` __`\/\ '__`\\\\ \ \   /'__`\     /'___\ / __`\ /' __` __`\ 
+\ \ \_/ \_/ \ \ \_/ \_/ \ \ \_/ \_/ \ __/\  __/\/>  <//\ \L\.\_/\ \/\ \/\ \ \ \L\ \\\\_\ \_/\  __/  __/\ \__//\ \L\ \/\ \/\ \/\ \ 
+ \ \___x___/'\ \___x___/'\ \___x___/'/\_\ \____\/\_/\_\ \__/.\_\ \_\ \_\ \_\ \ ,__//\____\ \____\/\_\ \____\ \____/\ \_\ \_\ \_\ 
+  \/__//__/   \/__//__/   \/__//__/  \/_/\/____/\//\/_/\/__/\/_/\/_/\/_/\/_/\ \ \/ \/____/\/____/\/_/\/____/\/___/  \/_/\/_/\/_/
+                                                                             \ \_\ 
+                                                                              \/_/
+'''
+
+WebSiteContents['www_example_net'] = '''\
+                                                                                       888                          888
+                                                                                       888                          888
+                                                                                       888                          888
+888  888  888888  888  888888  888  888    .d88b. 888  888 8888b. 88888b.d88b. 88888b. 888 .d88b.   88888b.  .d88b. 888888
+888  888  888888  888  888888  888  888   d8P  Y8b`Y8bd8P'    "88b888 "888 "88b888 "88b888d8P  Y8b  888 "88bd8P  Y8b888
+888  888  888888  888  888888  888  888   88888888  X88K  .d888888888  888  888888  88888888888888  888  88888888888888
+Y88b 888 d88PY88b 888 d88PY88b 888 d88Pd8bY8b.    .d8""8b.888  888888  888  888888 d88P888Y8b.   d8b888  888Y8b.    Y88b.
+ "Y8888888P"  "Y8888888P"  "Y8888888P" Y8P "Y8888 888  888"Y888888888  888  88888888P" 888 "Y8888Y8P888  888 "Y8888  "Y888
+                                                                               888
+                                                                               888
+                                                                               888
+'''
+
+WebSiteContents['www_example_edu'] = '''\
+                                                                                    dP                            dP
+                                                                                    88                            88
+dP  dP  dP dP  dP  dP dP  dP  dP    .d8888b. dP.  .dP .d8888b. 88d8b.d8b.  88d888b. 88 .d8888b.    .d8888b. .d888b88 dP    dP
+88  88  88 88  88  88 88  88  88    88ooood8  `8bd8'  88'  `88 88'`88'`88  88'  `88 88 88ooood8    88ooood8 88'  `88 88    88
+88.88b.88' 88.88b.88' 88.88b.88' dP 88.  ...  .d88b.  88.  .88 88  88  88  88.  .88 88 88.  ... dP 88.  ... 88.  .88 88.  .88
+8888P Y8P  8888P Y8P  8888P Y8P  88 `88888P' dP'  `dP `88888P8 dP  dP  dP  88Y888P' dP `88888P' 88 `88888P' `88888P8 `88888P'
+oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo~88~oooooooooooooooooooooooooooooooooooooooooooooooo
+                                                                           dP
+'''
 
 def run(dumpfile = None):
     ###############################################################################
@@ -40,7 +83,6 @@ def run(dumpfile = None):
         else:
             print(f"Usage:  {script_name} amd|arm")
             sys.exit(1)
-
 
 
     @dataclass
@@ -112,13 +154,13 @@ def run(dumpfile = None):
             GitRepo(repo_url = 'https://github.com/scionproto-contrib/http-proxy.git',
                     repo_branch = 'main',
                     repo_path = '/repos/http-proxy',
-                    notes='a caddy server module for SCION HTTP-proxy functionality' ),
+                    notes='SCION HTTP-proxy functionality' ),
 
 
             GitRepo(repo_url = 'https://github.com/scionproto-contrib/caddy-scion',
                     repo_branch = 'main',
                     repo_path = '/repos/caddy-scion',
-                    notes='caddy server plugins' )
+                    notes='caddy server plugins using the http-proxy module' )
 
             ]
 
@@ -385,7 +427,8 @@ def run(dumpfile = None):
         hnode = as_.getHost('host_0')
         dev_targets.append(hnode)
 
-
+    # on the docker-host with the SCION browser extensions do:
+    # sudo cp  /tmp/seedemu-minica-wtqc23j8/minica.pem /usr/local/share/ca-certificates/minica.crt && sudo update-ca-certificates
     caStore = RootMiniCAStore(caDomain='seedemu.internal.')
 
     caServer: MiniCAServer = minica.install('ca-vnode')
@@ -396,8 +439,12 @@ def run(dumpfile = None):
     caServer.installCACert(Filter())
 
     # 'entrypoint' into the simulation for browser-extension
+    # from docker-host access domains in the simulation through the forwarded port:
+    # curl "https://www.example.com:7443" --proxy "https://localhost:9443"
+    #            --proxy-header "Proxy-Authorization: Basic cG9saWN5Og==" 
     host_a = base.getAutonomousSystem(102).getHost('host_0')
     host_a.addPortForwarding(8888, 8888, 'tcp')
+    host_a.addPortForwarding(9443, 9443, 'tcp')
 
     sdns_server = sdns.install('sdns-vnode')
     sdns_server.setCAServer(caServer)
@@ -410,13 +457,18 @@ def run(dumpfile = None):
     fwdpxy.setCAServer(caServer)
     emu.addBinding(Binding('fwd_pxy', filter=Filter(asn=102, nodeName='host_0', allowBound=True)))
 
-    # HTTP web server and HTTP reverse proxy ...........................
+    # HTTP (SCION) web server ...........................
 
     # 'www.example.com'
     host_web_1 = base.getAutonomousSystem(172).getHost('host_0')
 
     w1 = web.install('web1')
     w1.enableHTTPS()
+    w1.setIndexContent(WebSiteContents['index_template'].format(address='1-172,10.172.0.71',
+                                                                nodeName='host_0',
+                                                                domain='www.example.com',
+                                                                body=WebSiteContents['www_example_com'],
+                                                                asn=172))
     w1.setServerNames(['www.example.com'])
     w1.setCAServer(caServer)
     emu.addBinding(Binding('web1', filter=Filter(asn=172, nodeName='host_0')))
@@ -426,6 +478,11 @@ def run(dumpfile = None):
 
     w2 = web.install('web2')
     w2.enableHTTPS()
+    w2.setIndexContent(WebSiteContents['index_template'].format(address='1-173,10.173.0.71',
+                                                                nodeName='host_0',
+                                                                domain='www.example.net',
+                                                                body=WebSiteContents['www_example_net'],
+                                                                asn=173))
     w2.setServerNames(['www.example.net'])
     w2.setCAServer(caServer)
     emu.addBinding(Binding('web2', filter=Filter(asn=173, nodeName='host_0')))
@@ -437,7 +494,13 @@ def run(dumpfile = None):
     w3.enableHTTPS()
     w3.setServerNames(['www.example.edu'])
     w3.setCAServer(caServer)
+    w3.setIndexContent(WebSiteContents['index_template'].format(address='2-241,10.241.0.71',
+                                                                nodeName='host_0',
+                                                                domain='www.example.edu',
+                                                                body=WebSiteContents['www_example_edu'],
+                                                                asn=241))
     emu.addBinding(Binding('web3', filter=Filter(asn=241, nodeName='host_0')))
+
 
 
     # coredns DoQ nameservers ..........................................
@@ -449,8 +512,6 @@ def run(dumpfile = None):
     root_a.setCAServer(caServer)
     root_a.addZone('.', createNsAndSoa=True).setMaster()
     emu.addBinding(Binding('root-a', filter=Filter(asn=235, nodeName='host_0', allowBound=True)))
-
-    #dns_svc.hostZoneOn()
 
     # 'com.' zone
     host_ns_2 = base.getAutonomousSystem(234).getHost('host_0')
@@ -524,8 +585,6 @@ def run(dumpfile = None):
     emu.addLayer(web)
     emu.addLayer(minica)
     emu.addLayer(devsvc)
-
-
 
 
     if dumpfile is not None:
